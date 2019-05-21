eda9f125ff2cc1bef0ae9ee0ba0bb5665e448e8a
darkniter <Poranot@yandex.ru>
2019-05-21 16:17:11 +0300

    change format

--- Changer.py
+++ Changer.py
@@ -5,6 +5,7 @@ houses = []
 hardware=[]
 result=[]
 town = 'Г. КУРОВСКОЕ'
+
 with open('houses.csv', newline='') as houses_read:
     houses_list = csv.reader(houses_read, delimiter=';', quotechar='|')
     
@@ -19,20 +20,24 @@ with open('hardware_ku.csv', newline='') as hardware_read:
 
     for init in hardware:
         street_hard = init[0].lower()
+
         try:
             number_hard = int(init[1])
         except BaseException:
             number_hard=None       
+
         for val in houses:
             street_house = val[2].lower()
+
             try:
                 number_house = int(val[3])
             except BaseException:
                 number_house=None
+
             if((street_house.find(street_hard,0) != -1)&(number_hard == number_house)&(val[1] == town)):
                 result.append(init[:3]+val[:4])
 
 for row in result:
-        print(row)    
+    print(row)    
 '''
 '''
