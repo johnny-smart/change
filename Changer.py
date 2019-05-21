52c77f7d26a58743a8a52c3d0089bd7cf4001744
darkniter <Poranot@yandex.ru>
2019-05-21 14:49:09 +0300

    Объединение записей при условии совпадения записей

--- Changer.py
+++ Changer.py
@@ -1,7 +1,9 @@
 import csv
+import string
 
 houses = []
 hardware=[]
+result=[]
 
 with open('houses.csv', newline='') as houses_read:
     houses_list = csv.reader(houses_read, delimiter=';', quotechar='|')
@@ -15,8 +17,23 @@ with open('hardware_ku.csv', newline='') as hardware_read:
     for row in hardware_list:
         hardware.append(row)
 
-    for init in hardware: 
-        print(type(init), init)
+    for init in hardware:
+        street_hard = init[0].lower()
+        try:
+            number_hard = int(init[1])
+        except BaseException:
+            number_hard=0        
+        for val in houses:
+            street_house = val[2].lower()
+            try:
+                number_house = int(val[3])
+            except BaseException:
+                number_house=0 
+            if((street_house.find(street_hard,0)!=-1)&(number_hard==number_house)):
+                result.append(init)
+                result.append(val)
 
+    for row in result:
+        print(row)    
 '''
 '''
