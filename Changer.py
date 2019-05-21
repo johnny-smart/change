051a455e94898d0d0515f0ce9a91331fc3654a2a
darkniter <Poranot@yandex.ru>
2019-05-21 10:32:46 +0300

    Загрузка из .csv файла и выборка по наименованию города

--- Changer.py
+++ Changer.py
@@ -1,4 +1,9 @@
-String ="вывод"
-str= String
-s= String+str
-print (s)
+import csv
+with open('C:\\Users\\smartintel\\Desktop\\Еремеев\\20190517-data_parser\\houses.csv', newline='') as houses:
+    houses_reader = csv.reader(houses, delimiter=';', quotechar='|')
+    for row in houses_reader:
+        if (row[1] == 'Г. КУРОВСКОЕ'):
+            print(type(row), row)
+    '''
+    
+    '''
