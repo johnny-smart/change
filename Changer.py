00a8089cfbca34359b3561b313725c352dbb38da
darkniter <Poranot@yandex.ru>
2019-05-24 13:55:18 +0300

    add main function

--- Changer.py
+++ Changer.py
@@ -1,70 +1,73 @@
 import csv
 import string
-i = 0
+
 houses = []
 hardware = []
 result = []
-town = 'Г. КУРОВСКОЕ'
-exceptions_result_house = []
-exceptions_result_hard = []
 
-with open('houses.csv', newline='') as houses_read:
-    houses_list = csv.reader(houses_read, delimiter=';', quotechar='|')
-    
-    for row in houses_list:
-        if(i != 0):
-            # row[]
-            houses.append(row)
-        i = i + 1   
 
-with open('hardware_ku.csv', newline='') as hardware_read:
-    hardware_list = csv.reader(hardware_read, delimiter=';', quotechar='|')
+def main(fname, town):
+    """
+    """
+    with open('houses.csv', newline='') as houses_read:
+        houses_list = csv.reader(houses_read, delimiter=';', quotechar='|')
 
-    i = 0
-    for raw in hardware_list:
-        if(i != 0):
-            hardware.append(raw)
-        i = i + 1
+        i = 0
+        for row in houses_list:
+            if(i != 0):
+                try:
+                    # number_house = int(val[3])
+                    number_house_arr = row[3].split()
+                    row[3] = number_house_arr[0]
+                    street_house = row[2].lower()
+                    row[2] = street_house
+                except BaseException:
+                    number_house = "_"
 
-    for init in hardware:
-        street_hard = init[0].lower()
+                try:
+                    if(number_house_arr[1].isalpha()):
+                        number_house = number_house_arr[1]
+                        number_house_arr[1] = number_house.lower()
+                        row[3] = "".join(number_house_arr)
+                except BaseException:
+                    pass
+                houses.append(row)
+            i += 1
 
-        try:
+    with open(fname, newline='') as hardware_read:
+        hardware_list = csv.reader(hardware_read, delimiter=';', quotechar='|')
+
+        i = 0
+        for row in hardware_list:
+            if(i != 0):
+                try:
+                    street_hard = row[0].lower()
+                    row[0] = street_hard
+                    number_hard_init = row[1].split()
+                    number_hard = number_hard_init[0].split('.')
+                    row[1] = number_hard[0]
+                except BaseException:
+                    number_hard = None
+                hardware.append(row)
+            i += 1
+
+        for init in hardware:
+            street_hard = init[0]
             number_hard = init[1]
-        except BaseException:
-            number_hard = None       
-        
 
-        for val in houses:
-            street_house = val[2].lower()
+            for val in houses:
+                street_house = val[2]
+                number_house = val[3]
 
-            try:
-                # number_house = int(val[3])
-                number_house_arr = val[3].split()
-                number_house = number_house_arr[0]
-              # 
-                #    
-            except BaseException:
-                number_house = "_"
+                if((street_house.find(street_hard, 0) != -1) &
+                   (number_hard == number_house) &
+                   (val[1] == town)):
+                    result.append(init[:3]+val[:4]+[number_hard, number_house])
 
-            try:
-                if(number_house_arr[1].isalpha()):
-                    number_house = number_house_arr[1]
-                    number_house_arr[1] = number_house.lower()
-                    number_house="".join(number_house_arr)
-            except BaseException:
-                pass
+    for row in result:
+        print(row)
+pass
 
-            if((street_house.find(street_hard,0) != -1) 
-                &(number_hard == number_house)
-                &(val[1] == town)):
-                result.append(init[:3]+val[:4]+[number_hard, number_house] )
-            # else:
-            #     print("ERR:", init[:3]+val[:4]+[number_hard, number_house])
-            
-
-for row in result:
-    print (row) 
-   
-'''
-'''
+if __name__ == "__main__":
+    main('hardware_ku.csv', 'Г. КУРОВСКОЕ')
+    main('hardware_ld.csv', 'Г. ЛИКИНО-ДУЛЕВО')
