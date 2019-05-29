87cf26303a23ad25313287f5f5f1eb01673a9770
darkniter <Poranot@yandex.ru>
2019-05-29 16:39:47 +0300

    Version on Excel

--- Changer.py
+++ Changer.py
@@ -1,31 +1,51 @@
+import xlrd
+import xlwt
 import csv
-import string
 
 
 def houses_init():  # функция для создания и заполнения массива записей домов
                     # (ввод: имя внутри функции, вывод: маcсив записей)
     houses = []
-    with open('houses.csv', newline='') as houses_read:  # чтение csv файла
+    '''with open('houses.csv', newline='') as houses_read:  # чтение csv файла
         next(houses_read, None)
         houses_list = csv.reader(houses_read, delimiter=';')
 
         for row in houses_list:  # заполнение массива данных
             pass
             houses.append(row)
-    houses_read.close()
+    houses_read.close()'''
+
+    houses_read = xlrd.open_workbook('houses.xlsx')
+    house_list = houses_read.sheet_by_index(0)
+
+    for row in range(house_list.nrows):
+        houses.append(house_list.row_values(row))
+
+    houses.pop(0)
+
     return(houses)
 
 
 def hardware_init(fname):
     hardware = []
-
+    '''
     with open(fname, newline='') as hardware_read:
         next(hardware_read, None)
         hardware_list = csv.reader(hardware_read, delimiter=';')
 
         for row in hardware_list:
             hardware.append(row)
-    hardware_read.close()
+    '''
+    hardware_read = xlrd.open_workbook(fname)
+    hardware_list = hardware_read.sheet_by_index(0)
+
+    for row in range(hardware_list.nrows):
+        # for row in hardware_list.get_rows():
+        hardware.append(hardware_list.row_values(row))
+        # hardware.append(row)
+
+    hardware.pop(0)
+
     return(hardware)
 
 
@@ -36,11 +56,7 @@ def result_init(houses, town, fname):
     for init in hardware:
         res_tmp = []
         try:
-            street_hard = init[1]
-            init[1] = street_hard
-            number_hard_init = init[2].split(',')
-            number_hard = number_hard_init[0].split('.')
-            init[2] = number_hard[0]
+            init[2] = ((str(init[2]).split(','))[0].split('.'))[0]
         except BaseException:
             pass
 
@@ -107,13 +123,13 @@ def main():
 
     houses = houses_init()
 
-    r = result_init(houses, 'Г. КУРОВСКОЕ', 'hardware_ku.csv')
+    r = result_init(houses, 'Г. КУРОВСКОЕ', 'hardware_ku.xlsx')
     out_file(r)
 
-    r = result_init(houses, 'Г. ЛИКИНО-ДУЛЕВО', 'hardware_ld.csv')
+    r = result_init(houses, 'Г. ЛИКИНО-ДУЛЕВО', 'hardware_ld.xlsx')
     out_file(r)
 
-    r = result_init(houses, 'Г. ОРЕХОВО-ЗУЕВО', 'hardware_oz.csv')
+    r = result_init(houses, 'Г. ОРЕХОВО-ЗУЕВО', 'hardware_oz.xlsx')
     out_file(r)
 
     # out_console(r)
