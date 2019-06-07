3383cfccb5eb345e9569b18e1855fbb70ec2bcde
darkniter <Poranot@yandex.ru>
2019-06-07 18:17:01 +0300

    fixed speed

--- Changer.py
+++ Changer.py
@@ -7,7 +7,7 @@ from os import path
 
 
 @timecall
-def houses_init():  # функция для создания и заполнения массива записей домов
+def houses_init(town):  # функция для создания и заполнения массива записей домов
                     # (ввод: имя внутри функции, вывод: маcсив записей)
     houses = []
     houses_wb = openpyxl.load_workbook('houses.xlsx')
@@ -19,7 +19,8 @@ def houses_init():  # функция для создания и заполнен
         cols = []
         for init in row:
             cols.append(init.value)
-        houses.append(cols)
+        if (cols[1] == town):
+            houses.append(cols)
     # print (w_sheet['B2'].value)
     return(houses)
 
@@ -34,20 +35,21 @@ def hardware_init(fname, sheet):
         cols = [None]*20
         for init in row:
             cols[init.column-1] = init.value
+            if (init.column == 10) or (init.column == 11):
+                cols[init.column-1] = str(init.value).split(' ')[0]
             if ((init.column == 1) and (init.font.strike is True)):
                 cols[19] = 1
-            elif (init.column == 1):
-                cols[19] = 0
             if (init.column == 4 or init.column == 5):
-                cols[18] = 1 if init.fill.fgColor.rgb == 'FF00B0F0' else 0
+                if(init.fill.fgColor.rgb == 'FF00B0F0'):
+                    cols[18] = 1
 
         hardware.append(cols)
     return(hardware)
 
 
 @timecall
-def result_init(houses, town, fname, sheet):
-
+def result_init(town, fname, sheet):
+    houses = houses_init(town)
     _err = []
     _double = []
     _result = []
@@ -57,12 +59,11 @@ def result_init(houses, town, fname, sheet):
     for init in hardware:
         res_tmp = []
         try:
-            init[1] = ((str(init[1]).split(','))[0].split('.'))[0]
+            number_hard = ((str(init[1]).split(','))[0].split('.'))[0]
         except BaseException:
             pass
 
         street_hard = init[0]
-        number_hard = init[1]
 
         for row in houses:
 
@@ -70,7 +71,6 @@ def result_init(houses, town, fname, sheet):
                 number_house_arr = row[3].split()
                 row[3] = number_house_arr[0]
                 street_house = row[2]
-                row[2] = street_house
             except BaseException:
                 print('err try')
 
@@ -91,6 +91,9 @@ def result_init(houses, town, fname, sheet):
             # if((street_house.lower().find(street_hard.lower(), 0) != -1) &
             #    (number_hard.lower() == number_house.lower()) &
             #    (row[1] == town)):
+            if (street_house == 'ул. .'):
+                street_house = row[1].lower().strip()
+
             if((street_house.find(street_hard, 0) != -1) &
                (number_hard == number_house) &
                (row[1] == town)):
@@ -99,7 +102,7 @@ def result_init(houses, town, fname, sheet):
 
         if not res_tmp:
             # print(init[:4])
-            _err += init
+            _err += [init]
         else:
             if (len(res_tmp) > 1):
                 _double += res_tmp
@@ -115,12 +118,6 @@ def result_init(houses, town, fname, sheet):
 
     print("result:", len(_result))
     return _result, _double, _err
-
-
-def out_console(result):
-    for row in result:
-        print(row)
-pass
 
 
 @timecall
@@ -144,33 +141,40 @@ def out_file(result, namefile):
 def main():
     errrec = []
     # geodata = []
-    result = []
+    result = [['P_STREET', 'P_HOUSE', 'P_MODEL', 'P_IP_OLD', 'P_IP',
+               'P_VECTOR', 'P_UPLINK', 'P_MAC', 'P_VLAN', 'P_DATE_SETUP',
+               'P_DATE_INSTALL', 'P_FLATS', 'P_DOOR', 'P_FLOOR',
+               'P_DESCRIPTION', 'P_RESERVED1', 'P_RESERVED2', 'P_RESERVED3',
+               'P_REMOVED', 'P_TRANSIT', 'P_HOUSE_ID']]
     double = []
-    houses = houses_init()
 
-    _result, _err, _double = result_init(houses, 'Г. КУРОВСКОЕ',
-                                                 'hardware_copy.xlsx',
-                                                 'Комутаторы КУ')
+    _result, _err, _double = result_init('Г. КУРОВСКОЕ',
+                                         'hardware_copy.xlsx',
+                                         'Комутаторы КУ')
     result += _result
     errrec += _err
     double += _double
 
-    _result, _err, _double = result_init(houses, 'Г. ЛИКИНО-ДУЛЕВО',
-                                                 'hardware_copy.xlsx',
-                                                 'Комутаторы ЛД')
+    _result, _err, _double = result_init('Г. ЛИКИНО-ДУЛЕВО',
+                                         'hardware_copy.xlsx',
+                                         'Комутаторы ЛД')
     result += _result
     errrec += _err
     double += _double
 
-    _result, _err, _double = result_init(houses, 'Г. ОРЕХОВО-ЗУЕВО',
-                                                 'hardware_copy.xlsx',
-                                                 'Комутаторы ОЗ')
+    _result, _err, _double = result_init('Г. ОРЕХОВО-ЗУЕВО',
+                                         'hardware_copy.xlsx',
+                                         'Комутаторы ОЗ')
     result += _result
     errrec += _err
     double += _double
 
-    # r = result_init(houses, 'Г. ОРЕХОВО-ЗУЕВО', 'hardware_copy.xlsx', 'Комутаторы ОЗ')
-    # r = result_init(houses, 'Г. ОРЕХОВО-ЗУЕВО', 'hardware_copy.xlsx', 'Комутаторы ОЗ')
+    _result, _err, _double = result_init('Д. КАБАНОВО',
+                                         'hardware_copy.xlsx',
+                                         'Комутаторы КБ')
+    result += _result
+    errrec += _err
+    double += _double
 
     out_file(result, 'result')
     out_file(errrec, 'Err')
