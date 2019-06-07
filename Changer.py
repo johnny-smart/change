884333b99246b1a091a0875b211763e128a5d5a0
darkniter <Poranot@yandex.ru>
2019-06-07 19:26:31 +0300

    fixed

--- Changer.py
+++ Changer.py
@@ -1,122 +1,192 @@
+import openpyxl
 import csv
-import string
+from profilehooks import timecall, profile
+# from geolocation import geoadressation
+from os import remove
+from os import path
 
 
+@timecall
 def houses_init():  # функция для создания и заполнения массива записей домов
                     # (ввод: имя внутри функции, вывод: маcсив записей)
     houses = []
-    with open('houses.csv', newline='') as houses_read:  # чтение csv файла
-        next(houses_read, None)
-        houses_list = csv.reader(houses_read, delimiter=';')
-
-        for row in houses_list:  # заполнение массива данных
-            pass
-            houses.append(row)
-    houses_read.close()
+    houses_wb = openpyxl.load_workbook('houses.xlsx')
+    houses_sn = houses_wb.sheetnames[0]
+    w_sheet = houses_wb[houses_sn]
+    houses_list = w_sheet.rows
+    next(houses_list)
+    for row in houses_list:
+        cols = []
+        for init in row:
+            cols.append(init.value)
+        houses.append(cols)
     return(houses)
 
 
-def hardware_init(fname):
+def houses_filter(town, houses):
+    houses_filtred = []
+    for row in houses:
+        if (row[1] == town):
+            houses_filtred.append(row)
+    return houses_filtred
+
+
+@timecall
+def hardware_init(fname, sheet):
     hardware = []
+    hardware_wb = openpyxl.load_workbook(fname)
+    hardware_list = hardware_wb[sheet].rows
+    next(hardware_list)
+    for row in hardware_list:
+        cols = [None]*20
+        for init in row:
+            cols[init.column-1] = init.value
 
-    with open(fname, newline='') as hardware_read:
-        next(hardware_read, None)
-        hardware_list = csv.reader(hardware_read, delimiter=';')
+            # if init.column == 2:
 
-        for row in hardware_list:
-            hardware.append(row)
-    hardware_read.close()
+            if ((init.column == 10) or (init.column == 11)) and (init.value is not None):
+                cols[init.column-1] = str(init.value).split(' ')[0]
+            if ((init.column == 1) and (init.font.strike is True)):
+                cols[19] = 1
+            if (init.column == 4 or init.column == 5):
+                if(init.fill.fgColor.rgb == 'FF00B0F0'):
+                    cols[18] = 1
+
+        hardware.append(cols)
     return(hardware)
 
 
-def result_init(houses, town, fname):
-    hardware = hardware_init(fname)
-    result = []
+@timecall
+def result_init(town, fname, sheet, houses):
+    houses_town = houses_filter(town, houses)
+    _err = []
+    _double = []
+    _result = []
+
+    hardware = hardware_init(fname, sheet)
     print("hardware:", len(hardware))
     for init in hardware:
         res_tmp = []
         try:
-            street_hard = init[1]
-            init[1] = street_hard
-            number_hard_init = init[2].split(',')
-            number_hard = number_hard_init[0].split('.')
-            init[2] = number_hard[0]
+            number_hard = ((str(init[1]).split(','))[0].split('.'))[0]
         except BaseException:
             pass
 
-        street_hard = init[1]
-        number_hard = init[2]
+        street_hard = init[0]
 
-        for row in houses:
+        for row in houses_town:
 
             try:
                 number_house_arr = row[3].split()
                 row[3] = number_house_arr[0]
                 street_house = row[2]
-                row[2] = street_house
             except BaseException:
                 print('err try')
 
             try:
-                if(number_house_arr[1].isalpha()):
+                if(number_house_arr[1].isalpha() or
+                                              ('/' in number_house_arr[1])):
                     row[3] = "".join(number_house_arr)
+
             except BaseException:
                 pass
 
             number_house = row[3]
 
-            if((street_house.lower().find(street_hard.lower(), 0) != -1) &
-               (number_hard.lower() == number_house.lower()) &
+            street_house = street_house.lower().strip()
+            street_hard = street_hard.lower().strip()
+            number_hard = number_hard.lower().strip()
+            number_house = number_house
+            if (street_house == 'ул. .'):
+                street_house = row[1].lower().strip()
+
+            if((street_house.lower().strip().find(street_hard.lower().strip(), 0) != -1) &
+               (number_hard.lower().strip() == number_house.lower().strip()) &
                (row[1] == town)):
                 # .append > =
-                res_tmp.append(init[:4]+row[:4]+[number_hard, number_house])
+                res_tmp.append([init[0]]+[str(init[1])]+init[2:]+[int(row[0])])
 
         if not res_tmp:
             # print(init[:4])
-            result.append(init)
+            _err += [init]
         else:
-            result.append(res_tmp)
+            if (len(res_tmp) > 1):
+                _double += res_tmp
+                # print(type(res_tmp[1]))
+            else:
+                _result += res_tmp
 
-    print("result:", len(result))
-    return(result)
+            # adress = (res_tmp[0][5] + ', ' + res_tmp[0][1] + ', ' +
+            #           res_tmp[0][2])
 
+            # location = geoadressation(adress)
+            # geodata.append([str(res_tmp[0][4])] + [location])
 
-def out_console(result):
-    for row in result:
-        print(row)
-pass
-
-
-def out_file(result):
-    try:
-        with open('result.csv', newline='') as newfile:
-            scvwr = csv.writer(newfile, delimiter=';')
-
-            for row in result:
-                scvwr.writerow(row)
-    except BaseException:
-        with open('result.csv', 'a+', newline='') as newfile:
-            scvwr = csv.writer(newfile, delimiter=';',
-                               quoting=csv.QUOTE_MINIMAL)
-
-            scvwr.writerows(result)
-        newfile.close()
+    print("result:", len(_result))
+    return _result, _double, _err
 
 
+@timecall
+def out_file(result, namefile):
+    # try:
+    #     with open(namefile + '.csv', newline='') as newfile:
+    #         scvwr = csv.writer(newfile, delimiter=';')
+    #         for row in result:
+    #             scvwr.writerow(row)
+    # except BaseException:
+
+    if path.isfile(namefile + '.csv'):
+        remove(namefile + '.csv')
+    with open(namefile + '.csv', 'a+', newline='') as newfile:
+        scvwr = csv.writer(newfile, delimiter=';', quoting=csv.QUOTE_MINIMAL)
+        scvwr.writerows(result)
+    newfile.close()
+
+
+@profile
 def main():
+    errrec = []
+    # geodata = []
+    result = [['P_STREET', 'P_HOUSE', 'P_MODEL', 'P_IP_OLD', 'P_IP',
+               'P_VECTOR', 'P_UPLINK', 'P_MAC', 'P_VLAN', 'P_DATE_SETUP',
+               'P_DATE_INSTALL', 'P_FLATS', 'P_DOOR', 'P_FLOOR',
+               'P_DESCRIPTION', 'P_RESERVED1', 'P_RESERVED2', 'P_RESERVED3',
+               'P_REMOVED', 'P_TRANSIT', 'P_HOUSE_ID']]
+    double = []
 
     houses = houses_init()
 
-    r = result_init(houses, 'Г. КУРОВСКОЕ', 'hardware_ku.csv')
-    out_file(r)
+    _result, _err, _double = result_init('Г. КУРОВСКОЕ',
+                                         'hardware_copy.xlsx',
+                                         'Комутаторы КУ', houses)
+    result += _result
+    errrec += _err
+    double += _double
 
-    r = result_init(houses, 'Г. ЛИКИНО-ДУЛЕВО', 'hardware_ld.csv')
-    out_file(r)
+    _result, _err, _double = result_init('Г. ЛИКИНО-ДУЛЕВО',
+                                         'hardware_copy.xlsx',
+                                         'Комутаторы ЛД', houses)
+    result += _result
+    errrec += _err
+    double += _double
 
-    r = result_init(houses, 'Г. ОРЕХОВО-ЗУЕВО', 'hardware_oz.csv')
-    out_file(r)
+    _result, _err, _double = result_init('Г. ОРЕХОВО-ЗУЕВО',
+                                         'hardware_copy.xlsx',
+                                         'Комутаторы ОЗ', houses)
+    result += _result
+    errrec += _err
+    double += _double
 
-    # out_console(r)
+    _result, _err, _double = result_init('Д. КАБАНОВО',
+                                         'hardware_copy.xlsx',
+                                         'Комутаторы КБ', houses)
+    result += _result
+    errrec += _err
+    double += _double
+
+    out_file(result, 'result')
+    out_file(errrec, 'Err')
+    out_file(double, 'Дубли')
 pass
 
 if __name__ == "__main__":
