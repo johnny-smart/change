da1db6ec997c5b15a357996d0beb084c5967c647
darkniter <Poranot@yandex.ru>
2019-12-23 14:47:50 +0300

    In the work (added the addresses handler from the hardware and KLADR)

--- err_decorator.py
+++ err_decorator.py
@@ -1,4 +1,5 @@
 from kladr_search import main as error_module
+from functools import partial
 
 def error_decorator(
     # err_rec, result, double
@@ -7,10 +8,12 @@ def error_decorator(
         print('Декоратор сработал arg')
         def result_decorator(town, fname, sheet, houses):
             _result, _err, _double = result_init(town, fname, sheet, houses)
-            re_search = (list(map(error_module, _err)))
+            error_module_fun = partial(error_module,town)
+            re_search = (list(map(error_module_fun, _err)))
             # result += _result
             # err_rec += _err
             # double += _double
+            print(re_search)
             return _result, _err, _double, re_search
         return result_decorator
     return decorator
--- kladr_search.py
+++ kladr_search.py
@@ -3,35 +3,59 @@ import json
 import re
 
 
-def main(address):
-    address_tmp = re.match(r'^(\d+)', address[1]).group(0)
+def main(town, address):
+    print(town)
+    address_number_tmp = re.match(r'^(\d+)', address[1]).group(0)
+    address_street_tmp = address[0].split('. ')
+
+    if len(address_street_tmp) == 1:
+        address_street_tmp.append('ул')
+        address_street_tmp.reverse()
+
     request = requests.get(
         'http://localhost:5000/api/v1/Kladr/',
-        params={'adress': " ".join([address[0],address_tmp])})
+        params={'adress': " ".join([address[0],address_number_tmp])})
     if request.ok:
         response = json.loads(request.text)
         res = response.get('result')
         if res:
-            for item in res:
-                if item.get('contentType') == 'building':
-                    item = format_kladr(item)
-                    if address[0].lower() == item[0].lower() and address[1].lower() == item[1].lower():
+            for item_kladr in res:
+                if item_kladr.get('contentType') == 'building':
+                    item_kladr = format_kladr(item_kladr)
+
+                    if (address_street_tmp[1].lower() == item_kladr[0].lower()
+                                and address[1].lower() == item_kladr[1].lower()
+                                and address_street_tmp[0].lower() == item_kladr[2]):
+
                         return [True, address]
     return [False, address]
+
+
+
 
 
 def format_kladr(item):
     for parent in item.get('parents'):
         if parent.get('contentType') == 'street':
-            return [parent.get('name'), format_item_addr(item.get('name'))]
+            return [parent.get('name'), format_item_addr(item.get('name')), parent.get('typeShort') ]
 
 
 def format_item_addr(addr):
-    addr = re.sub(r' корпус ','',addr)
-    addr = re.sub(r'')
-    # re.sub(r' строение ','/',addr)
+    addr = addr.split(" корпус ")
+
+    if len(addr) == 1:
+        addr = addr[0].split(" строение ")
+
+    if len(addr)>1 :
+        print(addr)
+        if addr[1].isdigit():
+           addr =  '/'.join(addr)
+
+    if type(addr) != "string":
+        addr = ''.join(addr)
+
     return addr
 
 
 if __name__ == "__main__":
-    main(['мая','21е'])
+    main('Орехово-Зуево',['мая','21е'])
